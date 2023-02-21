b0a67c5a1974fec1cb24ce63e770e091b98edded
Pedrofdez97 <pfernandezsalas@safareyes.es>
2023-02-21 17:36:00 +0100

    post perfil

--- src/app/profile/pages/user-profile/posts-profile/posts-profile.component.ts
+++ src/app/profile/pages/user-profile/posts-profile/posts-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../../../auth/services/auth.service";
+import {PostService} from "../../../services/post.service";
+import {ProfileService} from "../../../services/profile.service";
 
 @Component({
   selector: 'app-posts-profile',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./posts-profile.component.css']
 })
 export class PostsProfileComponent {
+  ngOnInit() {
+    this.listarPost();
+  }
+
+  get obtenerPost(){
+    return this.postService.usuarioPosts;
+  }
+
+  constructor(private authService: AuthService,
+              private postService: PostService,
+              private profileService: ProfileService) {
+  }
+
+
+  listarPost(){
+    const usermail = this.authService.usuario.username!;
+
+    this.postService.obtenerPosts(usermail)
+      .subscribe(resp =>{
+
+      })
+  }
+  get obtenerProfile(){
+    return this.profileService.profile;
+  }
 
 }
